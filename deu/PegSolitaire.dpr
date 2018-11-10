7f8cbd239f6fe5d8afb825b71d21f6d41119fc16
Daniel Marschall <info@daniel-marschall.de>
2018-11-10 21:46:45 +0000

    Using Language Resource DLLs compatible with Delphi 10.2
    git-svn-id: https://svn.viathinksoft.com/svn/jumper/trunk@5 f95cf3f4-14d6-4b0d-9104-497cf02f624c

--- deu/PegSolitaire.dpr
+++ deu/PegSolitaire.dpr
@@ -1,9 +1,7 @@
-// Nicht bearbeiten. Diese Datei wurde vom Ressourcen-DLL-Experten erzeugt.
+﻿// Nicht bearbeiten. Diese Datei wurde vom Ressourcen-DLL-Experten erzeugt.
 
 library PegSolitaire;
 
-{$R *.RES}
-{ITE} {LCID:00000409:00000407} {}
 {ITE} {DFMFileType} {About.dfm}
 {ITE} {DFMFileType} {Choice.dfm}
 {ITE} {DFMFileType} {Finish.dfm}
@@ -13,16 +11,16 @@ library PegSolitaire;
 {ITE} {DFMFileType} {Main.dfm}
 {ITE} {RCFileType} {PegSolitaire_DRC.rc}
 
-{$R 'About.dfm' AboutBox:TForm}
-{$R 'Choice.dfm' LevelChoice:TForm}
-{$R 'Finish.dfm' FinishForm:TForm}
-{$R 'Help.dfm' HelpForm:TForm}
-{$R 'HighScore.dfm' HighScoreForm:TForm}
-{$R 'History.dfm' HistoryForm:TForm}
-{$R 'Main.dfm' MainForm:TForm}
+{$R 'About.dfm' 'AboutBox:TForm'}
+{$R 'Choice.dfm' 'LevelChoice:TForm'}
+{$R 'Finish.dfm' 'FinishForm:TForm'}
+{$R 'Help.dfm' 'HelpForm:TForm'}
+{$R 'HighScore.dfm' 'HighScoreForm:TForm'}
+{$R 'History.dfm' 'HistoryForm:TForm'}
+{$R 'Main.dfm' 'MainForm:TForm'}
 {$R 'PegSolitaire_DRC.res' 'PegSolitaire_DRC.rc'}
 
-{$E deu}
+{$E DEU}
 
 begin
 end.
